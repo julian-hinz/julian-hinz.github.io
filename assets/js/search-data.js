45277543f87fc62d7dd640de886e212c031c408b
julian-hinz <36471582+julian-hinz@users.noreply.github.com>
2025-01-23 17:26:10 +0000

    Deploying to gh-pages from @ julian-hinz/julian-hinz.github.io@5bfd9e7c2f3459ffb9ad90269d2d79a0a264b940 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,14 +443,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%75%6C%69%61%6E.%68%69%6E%7A@%75%6E%69%67%65.%63%68", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/julian-hinz-630b0712b", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-5432-6263", "_blank");
         },
       },{
         id: 'social-rss',
@@ -464,14 +471,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=wb0a7GkAAAAJ&hl=en", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/_julianhinz", "_blank");
         },
       },{
       id: 'light-theme',
